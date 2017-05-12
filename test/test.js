b5fa02f2d637924f7949ca2a6bd7eae37c0c67ec
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-05-11 22:34:12 -0700

    start of main test

--- test/test.js
+++ test/test.js
@@ -13,6 +13,17 @@ const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
+function seedBlogData() {
+  console.info('seeding restaurant data');
+  const seedData = [];
+
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogData());
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
 function generateBlogData(){
      return{
           author: {
@@ -29,3 +40,26 @@ function tearDownDb() {
     console.warn('Deleting database');
     return mongoose.connection.dropDatabase();
 }
+
+describe ('Blog API resource',function(){
+
+      before(function() {
+        return runServer(TEST_DATABASE_URL);
+      });
+
+      beforeEach(function() {
+        return seedBlogData();
+      });
+
+      // zeroes out the db after each test has run
+      afterEach(function() {
+        return tearDownDb();
+      });
+
+      // ensures that if other test odules run after this they wont get an error upon restart
+      after(function() {
+        return closeServer();
+      });
+
+
+});
